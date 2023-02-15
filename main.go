3bf4583aacbcfaf05ac9b307d06034bf82aa0019
mdjahidanwar <mdjahidanwar1995@gmail.com>
2023-02-15 19:58:42 +0600

    variable declair and assigning value in one line

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var name string 
-	name = "jahid" 
+	var name = "jahid" 
 
 	fmt.Println("hello bangladesh")
 	fmt.Println("git add local to remote")
+	fmt.Println(name)
 }
