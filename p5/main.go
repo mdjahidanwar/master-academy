122cd5af28825c83daab402e85cffb05cf1a98b7
mdjahidanwar <mdjahidanwar1995@gmail.com>
2023-04-10 23:06:19 +0600

    findind variable types

--- p5/main.go
+++ p5/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println("hi")
+	x,y,z:="jahid",20,1995
+	fmt.Printf("%v=%T\n", x,x)
+	fmt.Printf("%v=%T\n", y,y)
+	fmt.Printf("%v=%T\n", z,z)
+	fmt.Println(reflect.TypeOf(x))
 }
